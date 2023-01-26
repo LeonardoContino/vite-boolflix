9043945827b9b3c7c63ab612dc52a644f5dce643
LeonardoContino <leoconti2000@hotmail.it>
2023-01-26 16:58:49 +0100

    aggiunto input con funzione getmovies

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,54 @@
 <script>
-import axios from 'axios'
+
+import axios from 'axios';
+  
+export default{
+  data(){
+    return{
+      searchTerm:'',
+      movies: [],
+      apiUri: 'https://api.themoviedb.org/3',
+      apiKey: '34d4c691860eb29357dea3f73d1e58f2'
+    }
+    
+  },
+  methods:{
+    GetMovies(query){
+      const config = {
+        params: {
+          api_key: this.apiKey,
+          query,  
+        }
+    }
+    axios.get(`${this.apiUri}/search/movie`, config)
+        .then(res => { this.movies = res.data.results; })
+        .catch(error => { console.log(error) })
+  },
+
+    
+  
+},
+
+}
+
 </script>
 
 <template>
+  <header class="container m-3 d-flex justify-content-end">
+  <input v-model="searchTerm" type="text">
+  <button  @click="GetMovies(searchTerm)">cerca</button>
+</header>
 
+<main>
+  <ul v-for="movie in movies" @key="movies.id">
+    <li>titolo - {{ movie.title }}</li>
+    <li>titolo originale - {{ movie.original_title }}</li>
+    <li>lingua - {{ movie.original_language }}</li>
+    <li>valutazione - {{ movie.vote_average }}</li>
+    <li>trama - {{ movie.overview }}</li>
+
+  </ul>
+</main>
 </template>
 
 <style scoped>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<script>
+import axios from 'axios'
+</script>
+
+<template>
+
+
+<main></main>
+</template>
+
+<style scoped>
+
+</style>
