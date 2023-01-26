02789dafbddb18adda8c03ba0db7622dff2fb4ea
LeonardoContino <leoconti2000@hotmail.it>
2023-01-26 16:04:39 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import bootstrap from "bootstrap/dist/css/bootstrap.min.css"
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import App from './App.vue'
+
+createApp(App).mount('#app')
