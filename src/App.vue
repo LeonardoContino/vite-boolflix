19a218682569dd584b98fb9f3c77f5981d4e53bc
LeonardoContino <leoconti2000@hotmail.it>
2023-01-27 16:17:47 +0100

    aggiunto serie

--- src/App.vue
+++ src/App.vue
@@ -7,27 +7,43 @@ export default{
     return{
       searchTerm:'',
       movies: [],
+      series:[],
       apiUri: 'https://api.themoviedb.org/3',
       apiKey: '34d4c691860eb29357dea3f73d1e58f2'
     }
     
   },
   methods:{
-    GetMovies(query){
-      const config = {
+    GetLanguageImg(img) {
+   const url = new URL(`./assets/img/${img}.png`, import.meta.url)
+    return url 
+
+      
+    },
+    searchProductions(query){
+      if(!this.searchTerm){
+        this.movies = [];
+        this.series = [];
+        return;
+      }
+      this.fetchApi('search/movies', 'movies');
+      this.fetchApi('search/tv', 'series');
+
+    },
+      fetchApi(endpoint,collection){
+        const config = {
         params: {
           api_key: this.apiKey,
-          query,  
-        }
-    }
-    axios.get(`${this.apiUri}/search/movie`, config)
-        .then(res => { this.movies = res.data.results; })
+          query: this.searchTerm  
+        }}
+    
+    axios.get(`${this.apiUri}/${endpoint}`, config)
+        .then(res => { this[collection] = res.data.results; })
         .catch(error => { console.log(error) })
-  },
-  GetLenguageImg(img) {
-    return url = new URL(`./assets/img/${img}.png`)
+      }
       
-    }
+  
+  
 
     
   
@@ -39,19 +55,34 @@ export default{
 
 <template>
   <header class="container m-3 d-flex justify-content-end">
-  <input v-model="searchTerm" type="text">
-  <button  @click="GetMovies(searchTerm)">cerca</button>
+  <input v-model.trim="searchTerm" type="text">
+  <button  @click="searchProductions(searchTerm)">cerca</button>
 </header>
 
 <main>
-  <ul v-for="movie in movies" @key="movies.id">
+  <section>
+  <h3>movies</h3>
+  <ul v-for="movie in movies" @key="movie.id">
     <li>titolo - {{ movie.title }}</li>
     <li>titolo originale - {{ movie.original_title }}</li>
-    <li>lingua - <img :src="GetLenguageImg(movie.original_language)" :alt="movie.original_language"></li>
+    <li>lingua - <img :src="GetLanguageImg(movie.original_language)" :alt="movie.original_language"></li>
     <li>valutazione - {{ movie.vote_average }}</li>
     <li>trama - {{ movie.overview }}</li>
 
   </ul>
+</section>
+
+<section>
+  <h3>series</h3>
+  <ul v-for="serie in series" @key="serie.id">
+    <li>titolo - {{ serie.name }}</li>
+    <li>titolo originale - {{ serie.original_name }}</li>
+    <li>lingua - <img :src="GetLanguageImg(serie.original_language)" :alt="serie.original_language"></li>
+    <li>valutazione - {{ serie.vote_average }}</li>
+    <li>trama - {{ serie.overview }}</li>
+
+  </ul>
+</section>
 </main>
 </template>
 
